493866919d44055e164e03acb734da167e5390da
Prosper24 <61100701+Prosper24@users.noreply.github.com>
2020-02-23 08:14:07 -0800

    Add files via upload

---
+++ SalesPrediction.py
@@ -0,0 +1,9 @@
+#Get the projected amount of total sales
+#Display the profit
+
+
+projected_amount = float(input('Enter the projected amount of total sales:$'))
+profit = 0.23 * projected_amount
+
+print('The Profit made from that amount is $'+
+      format(profit, '.2f'))
